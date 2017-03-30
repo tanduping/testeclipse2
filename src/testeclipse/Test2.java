686922b9608f7a4a16a1b5de3354293b40e1e20f
tanduping <tanduping@jd.com>
2017-03-30 11:02:52 +0800

    提交：eclipse2

--- src/testeclipse/Test2.java
+++ src/testeclipse/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("eclipse 1>2>web1");
+		System.out.println("eclipse 2");
 	}
 
 }
